9f09123f91227331b75224ad51eb1fe11eb23a9e
Alexander Rusetsky <alex_rusk@mail.ru>
2017-09-03 00:53:15 +0300

    after add nuget azure storage

---
+++ itstep-webapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="itstep_webapp.MvcApplication" Language="C#" %>
---
+++ itstep-webapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(itstep_webapp.Startup))]
+namespace itstep_webapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
